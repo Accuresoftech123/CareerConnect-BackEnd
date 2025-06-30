95fb4a96ab1898c42a7da9c61c9b14ff08c83d29
Sambhajibhadre <sambhajibhadre777@gmail.com>
2025-06-30 10:39:11 +0530

    employer registration complete

--- src/main/java/com/example/controller/RecruiterController.java
+++ src/main/java/com/example/controller/RecruiterController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/recruiters") // Plural for RESTful consistency
+@CrossOrigin(origins = "http://localhost:3000")
 public class RecruiterController {
 
 	@Autowired
